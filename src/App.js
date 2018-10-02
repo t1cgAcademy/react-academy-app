7f9d4320fa15871241bb1aa28351fd89b8b1c529
Anthony DeCarlo <toekneerd@gmail.com>
2018-10-02 16:21:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import t1cgLogo from './t1cgLogo.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={t1cgLogo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React Academy App</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit
+          <code> src/App.js </code>
+          and save to reload.
+        </p>
       </div>
     );
   }
