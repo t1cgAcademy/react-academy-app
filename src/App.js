cd8479d8c32dc9079541588f9a6e00f385c5c44d
steve <steve@Steves-MBP.fios-router.home>
2018-11-17 10:11:10 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React, {Component} from 'react';
-import t1cgLogo from './t1cgLogo.png';
+import React, { Component } from 'react';
+import PlayerCard from './PlayerCard.jsx';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={t1cgLogo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React Academy App</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code> src/App.js </code>
-          and save to reload.
-        </p>
+        <PlayerCard />
       </div>
     );
   }
