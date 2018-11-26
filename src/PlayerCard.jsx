9ccbb00dd58998f3a694626f6264fd9bd7053da8
steve <steve@Steves-MBP.fios-router.home>
2018-11-26 17:01:47 -0500

    Revert "First commit"
    This reverts commit cd8479d8c32dc9079541588f9a6e00f385c5c44d.

--- src/PlayerCard.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import './App.css';
-
-const PlayerCard = () => {
-  return <div className="player-card">Sanity Again</div>;
-};
-
-export default PlayerCard;
